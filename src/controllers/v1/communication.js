0854989f112378aa2950735ad045c04f6e9dc99b
nevil <nevil@tunerlabs.com>
2024-11-12 22:31:06 +0530

    refactored: communication controller

--- src/controllers/v1/communication.js
+++ src/controllers/v1/communication.js
@@ -3,8 +3,7 @@ const communicationService = require('@services/communication')
 module.exports = class Communication {
 	async signup(req) {
 		try {
-			const sendEmail = await communicationService.signup(req.body)
-			return sendEmail
+			return await communicationService.signup(req.body)
 		} catch (error) {
 			return error
 		}
@@ -12,32 +11,28 @@ module.exports = class Communication {
 
 	async login(req) {
 		try {
-			const sendEmail = await communicationService.login(req.body)
-			return sendEmail
+			return await communicationService.login(req.body)
 		} catch (error) {
 			return error
 		}
 	}
 	async logout(req) {
 		try {
-			const sendEmail = await communicationService.logout(req.body)
-			return sendEmail
+			return await communicationService.logout(req.body)
 		} catch (error) {
 			return error
 		}
 	}
 	async createRoom(req) {
 		try {
-			const sendEmail = await communicationService.createRoom(req.body)
-			return sendEmail
+			return await communicationService.createRoom(req.body)
 		} catch (error) {
 			return error
 		}
 	}
 	async updateAvatar(req) {
 		try {
-			const sendEmail = await communicationService.updateAvatar(req.body.user_id, req.body.image_url)
-			return sendEmail
+			return await communicationService.updateAvatar(req.body.user_id, req.body.image_url)
 		} catch (error) {
 			return error
 		}
