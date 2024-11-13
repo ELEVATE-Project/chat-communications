a5f1df927f3faa1a419e1f58ecbef49b3de854de
nevil <nevil@tunerlabs.com>
2024-11-13 11:51:42 +0530

    updated: build.sh and utils

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ echo "Docker build script"
 set -eo pipefail
 
 build_tag=$1
-name=mentoring-notification-service
+name=mentoring-chat-communications-service
 node=$2
 org=$3
 
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 
-echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
+echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} >metadata.json
--- src/generics/utils.js
+++ src/generics/utils.js
@@ -2,8 +2,8 @@
 const crypto = require('crypto')
 
 // Retrieve environment variables and parse lengths as integers
-const USERNAME_HASH_SALT = process.env.USERNAME_HASH_SALT || ''
-const PASSWORD_HASH_SALT = process.env.PASSWORD_HASH_SALT || ''
+const USERNAME_HASH_SALT = process.env.USERNAME_HASH_SALT
+const PASSWORD_HASH_SALT = process.env.PASSWORD_HASH_SALT
 const USERNAME_HASH_LENGTH = parseInt(process.env.USERNAME_HASH_LENGTH, 10) || 8
 const PASSWORD_HASH_LENGTH = parseInt(process.env.PASSWORD_HASH_LENGTH, 10) || 8
 
