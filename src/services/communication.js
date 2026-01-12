02272bfa34f6ad09149e8d8749e10a1adce78b38
sumanvpacewisdom <suman.v@pacewisdom.com>
2026-01-12 18:20:43 +0530

    Remove Logs

--- src/services/communication.js
+++ src/services/communication.js
@@ -318,12 +318,6 @@ module.exports = class CommunicationHelper {
 		const tenantCode = bodyData.tenant_code
 		const externalUserId = bodyData.external_user_id
 
-		console.log('tenantCode', tenantCode)
-		console.log('externalUserId', externalUserId)
-		console.log('bodyData', bodyData)
-		console.log('bodyData.tenant_code', bodyData.tenant_code)
-		console.log('bodyData.external_user_id', bodyData.external_user_id)
-		console.log('bodyData.tenant_code', bodyData.tenant_code)
 		// Validate required parameters
 		if (!tenantCode) {
 			return responses.failureResponse({
