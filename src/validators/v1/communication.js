37808707cd57fc42b7db5a4f3a5fb1c882ad5205
sumanvpacewisdom <suman.v@pacewisdom.com>
2026-03-24 22:10:09 +0530

    removed validators for email

--- src/validators/v1/communication.js
+++ src/validators/v1/communication.js
@@ -8,7 +8,6 @@ module.exports = {
 			.isString()
 			.withMessage('User ID must be a string.'),
 		check('name').notEmpty().withMessage('Name is required.').isString().withMessage('Name must be a string.'),
-		check('email').optional().isEmail().withMessage('Invalid email format.'),
 		check('image_url').optional().isURL().withMessage('Image URL must be a valid URL.'),
 	],
 	login: [
